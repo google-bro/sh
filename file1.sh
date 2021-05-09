cf38838c2c398babd5a73c0fb3abddae316a085f
google-bro <83259724+google-bro@users.noreply.github.com>
2021-05-09 15:41:48 +0530

    Create file1.sh

---
+++ file1.sh
@@ -0,0 +1,5 @@
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install ubuntu-desktop
+wget  https://download.nomachine.com/download/7.4/Linux/nomachine_7.4.1_1_amd64.deb
+sudo apt install ./nomachine_7.4.1_1_amd64.deb
